85b1733b77fadbfe9b167f6b379625d1d9dbbeba
BlogTrafficExchange <BlogTrafficExchange@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-30 13:52:48 +0000

    Updated to WordPress 3.1
    git-svn-id: https://plugins.svn.wordpress.org/old-post-promoter/trunk@366940 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- OldPostPromoter.php
+++ OldPostPromoter.php
@@ -3,7 +3,7 @@
 Plugin Name: Old Post Promoter (by BTE)
 Plugin URI: http://www.blogtrafficexchange.com/old-post-promoter
 Description: Randomly choose an old post and reset the publication date to now.  The effect is to promote older posts by moving them back onto the front page and into the rss feed.  This plugin should only be used with data agnostic permalinks (permalink structures not containing dates). <a href="options-general.php?page=BTE_OPP_admin.php">Configuration options are here.</a>  "You down with OPP?  Yeah you know me!" 
-Version: 1.9.0
+Version: 1.9.1
 Author: Blog Traffic Exchange
 Author URI: http://www.blogtrafficexchange.com/
 Donate: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=1777819
